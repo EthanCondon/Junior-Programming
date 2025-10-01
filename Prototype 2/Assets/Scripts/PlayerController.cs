dec7debe3c0fbf3355a2c61995b7d4b6c4650b1f
EthanCondon <ethancondon735@gmail.com>
2025-10-01 08:29:07 -0400

    10/1/25

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	public GameObject projectilePrefab;
 	public float horizontalInput;
 	public float speed = 10.0f;
 	public float xRange = 10;
@@ -26,5 +27,11 @@ public class PlayerController : MonoBehaviour
 
         horizontalInput = Input.GetAxis("Horizontal");
 	transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+
+	if (Input.GetKeyDown(KeyCode.Space))
+	{
+	Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+	}
     }
 }
