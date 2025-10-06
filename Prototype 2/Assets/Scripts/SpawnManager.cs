953eb78d61b218abe682cc2fe728d0b7d6d6dc7e
EthanCondon <ethancondon735@gmail.com>
2025-10-06 08:32:53 -0400

    Collision Decisions

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -3,26 +3,25 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
 	public GameObject[] animalPrefabs;
-	private float spawnRangeX = 20;
-	private float spawnPosZ = 20;
+	private float xSpawnRange = 20;
+	private float zSpawnPos = 20;
+	private float startDelay = .5f;
+	private float spawnInterval = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
 	{
-	if (Input.GetKeyDown(KeyCode.S)) 
-    {
-        int animalIndex = Random.Range(0, animalPrefabs.Length);
-
 	
-	Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-
-	Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation); 
-    }
+    
 }
-}
+void SpawnRandomAnimal() {
+	int animalIndex = Random.Range(0, animalPrefabs.Length);
+	Vector3 spawnpos = new Vector3(Random.Range(-xSpawnRange, xSpawnRange), 0, zSpawnPos);
+	Instantiate(animalPrefabs[animalIndex], spawnpos, animalPrefabs[animalIndex].transform.rotation);
+}}
