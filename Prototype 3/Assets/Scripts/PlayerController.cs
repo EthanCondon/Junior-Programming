66049414b8d427f83322e094a811bdc152fd7935
EthanCondon <ethancondon735@gmail.com>
2025-10-09 08:25:13 -0400

    Don't Just Stand There

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -7,21 +7,23 @@ public class PlayerController : MonoBehaviour
 	public float gravityModifier;
 	public bool isOnGround = true;
 	public bool gameOver = false;
-
+	private Animator playerAnim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+	playerAnim = GetComponent<Animator>();
 	Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) {
 	playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
-	isOnGround = false; }
+	isOnGround = false;
+	playerAnim.SetTrigger("Jump_trig"); }
     }
 
 	private void OnCollisionEnter(Collision collision) {
@@ -29,5 +31,7 @@ public class PlayerController : MonoBehaviour
 	isOnGround = true; }
 	else if (collision.gameObject.CompareTag("Obstacle")) {
 	gameOver = true;
-	Debug.Log("Game Over!"); } }
+	Debug.Log("Game Over!"); 
+	playerAnim.SetBool("Death_b", true); 
+	playerAnim.SetInteger("DeathType_int", 1); } }
 }
