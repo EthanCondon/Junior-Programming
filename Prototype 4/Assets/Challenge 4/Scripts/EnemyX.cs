842175affabf9f4ef97e26d0a87bfc8120a73f04
EthanCondon <ethancondon735@gmail.com>
2025-10-21 08:18:02 -0400

    Soccer Scripting

---
+++ Prototype 4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+    }
+
+    void Update()
+    {
+        if (playerGoal == null) return;
+
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed); // removed * Time.deltaTime
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Enemy Goal" || other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype 4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float speed = 900f;
+    private GameObject focalPoint;
+
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 5;
+
+    private float normalStrength = 10f; 
+    private float powerupStrength = 25f; 
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
+
+        // Keep powerup indicator under player
+        if (powerupIndicator != null)
+            powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown()); // <-- you forgot this line before
+        }
+    }
+
+    private IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRb = other.rigidbody;
+            if (enemyRb == null) return;
+
+            // FIXED: direction should be away from the player
+            Vector3 awayFromPlayer = other.transform.position - transform.position;
+
+            float force = hasPowerup ? powerupStrength : normalStrength;
+            enemyRb.AddForce(awayFromPlayer.normalized * force, ForceMode.Impulse);
+
+            Debug.Log($"Hit {other.gameObject.name} with force {force}");
+        }
+    }
+}
