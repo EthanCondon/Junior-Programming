d08eacc9eef5878acdf81ae7c9bc041724ddad98
EthanCondon <ethancondon735@gmail.com>
2025-11-04 08:40:42 -0500

    11/4/25

--- Prototype 5/Assets/Course Library/Scripts/GameManager.cs
+++ Prototype 5/Assets/Course Library/Scripts/GameManager.cs
@@ -1,17 +1,32 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
 public class GameManager : MonoBehaviour 
+
 { 
 public List<GameObject> targets; 
 private float spawnRate = 1.0f; 
+private int score;
+public TextMeshProUGUI scoreText;
+
 void Start() { 
 
 StartCoroutine(SpawnTarget()); 
+score = 0;
+UpdateScore(0);
 
+    scoreText.text = "HELLO FROM HELL";
+    scoreText.color = Color.red;
 } 
 
-IEnumerator SpawnTarget() { while (true) { yield return new WaitForSeconds(spawnRate); int index = Random.Range(0, targets.Count); Instantiate(targets[index]); } }
+IEnumerator SpawnTarget() { while (true) { yield return new WaitForSeconds(spawnRate); int index = Random.Range(0, targets.Count); Instantiate(targets[index]);
+UpdateScore(5); } }
+
+private void UpdateScore(int scoreToAdd) {
+	score += scoreToAdd;
+	scoreText.text = "Score: " + score; }
 
 private void OnMouseDown() { Destroy(gameObject); } 
 
